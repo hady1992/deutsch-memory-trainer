35d8a7fa13d7ebad61f458cf95cd4f66551ab094
hady1992 <hady1992@example.com>
2026-06-30 20:20:45 +0200

    Fix Vite dev esbuild target

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react';
+﻿import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
@@ -21,7 +21,15 @@ export default defineConfig(() => {
       target: 'es2022',
     },
 
+    optimizeDeps: {
+      esbuildOptions: {
+        target: 'es2022',
+      },
+    },
+
     server: {
+      port: 3000,
+      host: '0.0.0.0',
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
